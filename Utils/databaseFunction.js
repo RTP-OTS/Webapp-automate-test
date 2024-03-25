30886f5b9d813c54ec83aa9e510e691e57ddc20c
RTP-OTS <rattapon.ot@gmail.com>
2024-03-25 19:39:57 +0700

    update

--- Utils/databaseFunction.js
+++
@@ -1,7 +0,0 @@
-const { queryDatabase } = require('../resources/utils/databaseUtils');
-
-async fetchEmployeeFromDatabase(employeeID){
-    const testdata = await queryDatabase(employeeID);
-    //console.log(testdata);
-    return testdata[0];
-}
--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,24 +1,23 @@
 import { test, expect } from '@playwright/test';
 const { queryDatabase } = require('../Utils/databaseUtils');
-const testData = require('../testdata/testData.json');
 
 async function fetchDataFromDatabase(staffname) {
     const staffData = await queryDatabase(staffname); 
     return staffData[0]; 
 }
 
-test.only('Use database for testing', async ({page}) => {
-    const staffname = testData.staffName;
-    const userDetails = await fetchDataFromDatabase(staffname);
+test('Use database for testing', async ({page}) => {
+    const userDetails = await fetchDataFromDatabase('Bob');
     console.log(userDetails)
 
     await page.goto('http://localhost:3000/login');
-    await page.locator('#username').fill(userDetails.username); // Ensure 'userDetails' includes 'username'
-    await page.locator('#password').fill(testData.password); // Assuming 'userDetails' includes 'password'
+    await page.pause();
+    await page.locator('#username').fill(userDetails.username);
+    await page.locator('#password').fill('123456');
     await page.locator('#login-button').click();
 
 
     const getNameText = await page.locator('#fname').textContent();
-    console.log(`${getNameText}`);
+    console.log(`Firstname staff is : ${getNameText}`);
     expect(getNameText).toContain(userDetails.first_name);
 });
